bad7529bf6c7b4aafa6a5172983502d2f2765d8b
Nicefa <nicefaanalytics@gmail.com>
2025-11-25 10:09:07 +0200

    Delete app directory

--- app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Create a blueprint for user-facing routes
-bp = Blueprint("main", __name__)
-
-# Import the user routes so they register with the blueprint
-from . import user_routes, admin_routes
